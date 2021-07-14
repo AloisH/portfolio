f532ad978cc78d00c4e8db434e251e2e873263ab
alois heloir <alois.heloir@live.fr>
2021-07-14 12:09:15 +0200

    Feature(Header): Add Navigation
    I added the navigation bar,
    In the same time I have setup the project mostly styles.css
    
    I also added svg icon, my cv in pdf and my face as a png picture.
    
    Finally, I added a dark/light theme when you click on a button

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  isLight : boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  changeTheme() {
+
+    if (this.isLight) {
+      document.documentElement.style.setProperty('--background-color', '#000000');
+      document.documentElement.style.setProperty('--text-color', '#FDFDFD');
+      document.documentElement.style.setProperty('--outline', '#C4C4C4');
+      document.documentElement.style.setProperty('--main-background-color', "#0C0C0C")
+
+      this.isLight = false;
+      return;
+    }
+
+    document.documentElement.style.setProperty('--background-color', '#F9F9F9');
+    document.documentElement.style.setProperty('--text-color', '#000000');
+    document.documentElement.style.setProperty('--outline', '#CFCFCF');
+    document.documentElement.style.setProperty('--main-background-color', "#F6F6F6")
+    
+    this.isLight = true;
+  }
+
+}
